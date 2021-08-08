22d812730da581e4b07f998637fb715c4230db50
avrona <alex@pointthegap.com>
2021-08-08 10:08:10 +0200

    Create fr.php
    Create a beta translation fr.php file from en.php.

---
+++ src/translations/fr.php
@@ -0,0 +1,31 @@
+<?php
+$translations = array(
+    'name' => 'Nom',
+    'email' => 'E-Mail',
+    'comment' => 'Commentaire',
+
+    'emailHint' => 'L'E-Mail est optionneL S'il est fourni, il sera utilisé pour envoyer des notifications de réponse au commentaire. L'E-Mail n'est jamais publié. Vous pourrez dépublier votre commentaire ou effacer votre adresse E-Mail grâce aux notifications.',
+
+    'submit' => 'Publier',
+    'reply' => 'Répondre',
+    'writeComment' => 'Ecrivez un commentaire...',
+
+    'noCommentsYet' => 'Pas encore de commentaires. Soyez le premier!',
+    'successMessage' => 'Commentaire publié avec succès.',
+    'failMessage' => "Impossible de publier votre commentaire. Raison: ",
+
+    'dateString' => 'Il y a {}',
+    'years' => 'années',
+    'months' => 'mois',
+    'days' => 'jours',
+    'hours' => 'heures',
+    'minutes' => 'minutes',
+    'seconds' => 'secondes',
+
+    # reply notification e-mail
+    'subject' => 'Reply to your comment by {}',
+    'introduction' => 'Il y a une réponse à votre commentaire.',
+    'author' => 'Auteur',
+    'message' => 'Message',
+    'unsubscribeDescription' => 'Pour se désabonner de ces notifications E-Mail, veuillez cliquer sur le lien suivant:',
+);
